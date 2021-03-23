be99de181bda344025701b8dcdf6b2ca61fcf715
Usuario <Usuario@DESKTOP-LLHTSEL>
2021-03-22 23:28:17 -0500

    Proyecto(1-2)

--- src/com/senati/eti/caso14.java
+++ src/com/senati/eti/caso14.java
@@ -7,10 +7,23 @@ public class caso14 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Primer Número: ");
-		int n1 = sc.nextInt();
+		int n, x = 0;
 		
+		System.out.print("Ingrese número: ");
+		n = sc.nextInt();
+		System.out.println("==================");
+		System.out.println("=== RESULTADOS ===");
+		System.out.println("==================");
 		
+		if(n > 0) {
+			for(int i = 1; i <= n; i++) {
+				x += i;
+			}
+			
+			System.out.println("Sumatoria : " + x);
+		}else {
+			System.out.println("El valor introducido no es mayor a 0");
+		}	
 
 	}
 
